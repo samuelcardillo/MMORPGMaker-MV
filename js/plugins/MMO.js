9942e10530c18b8718f3efe074c6da8066ca246e
Samuel Cardillo <iam@samuelcardillo.com>
2019-11-07 20:14:44 +0000

    Easily changeable server IP

--- js/plugins/MMO.js
+++ js/plugins/MMO.js
@@ -7,13 +7,22 @@
  * @author Samuel LESPES CARDILLO
  *
  * @help This plugin does not provide plugin commands.
+ * 
+ * @param Server Location
+ * @desc Server address (+ port)
+ * @default 127.0.0.1:8097
  */
 
+var MMO = MMO || {};
 var socket = undefined;
 var isLogged = false;
 var isMoving = false;
 var players = {};
 
 (function() {
-  socket = socket || io.connect('http://127.0.0.1:8097/');
+  MMO.Parameters = PluginManager.parameters('MMO');
+  let serverAddress = String(MMO.Parameters['Server Location']); // 
+
+
+  socket = socket || io.connect('http://' + serverAddress + '/');
 })();
