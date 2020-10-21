4bb44b5f46eb70c276815556ec016a031fd5c13f
Anda <ax.fiolle@gmail.com>
2020-10-21 16:14:43 +0200

    [KEYBOARD] Added WASD/ZQSD

--- js/rmmz_core.js
+++ js/rmmz_core.js
@@ -5594,30 +5594,33 @@ Input.keyRepeatInterval = 6;
  * @type Object
  */
 Input.keyMapper = {
-    9: "tab", // tab
-    13: "ok", // enter
-    16: "shift", // shift
-    17: "control", // control
-    18: "control", // alt
-    27: "escape", // escape
-    32: "ok", // space
-    33: "pageup", // pageup
-    34: "pagedown", // pagedown
-    37: "left", // left arrow
-    38: "up", // up arrow
-    39: "right", // right arrow
-    40: "down", // down arrow
-    45: "escape", // insert
-    81: "pageup", // Q
-    87: "pagedown", // W
-    88: "escape", // X
-    90: "ok", // Z
-    96: "escape", // numpad 0
-    98: "down", // numpad 2
-    100: "left", // numpad 4
-    102: "right", // numpad 6
-    104: "up", // numpad 8
-    120: "debug" // F9
+    9: 'tab',       // tab
+    13: 'ok',       // enter (You can comment this line to prevent Enter key Action/Chatbox conflicts)
+    16: 'shift',    // shift
+    17: 'control',  // control
+    18: 'control',  // alt
+    27: 'escape',   // escape
+    32: 'ok',       // space
+    33: 'pageup',   // pageup
+    34: 'pagedown', // pagedown
+    37: 'left',     // left arrow
+    38: 'up',       // up arrow
+    39: 'right',    // right arrow
+    40: 'down',     // down arrow
+    45: 'escape',   // insert
+    65: 'left',     // A
+    68: 'right',    // D
+    81: 'left',     // Q
+    83: 'down',     // S
+    87: 'up',       // W
+    88: 'escape',   // X
+    90: 'up',       // Z
+    96: 'escape',   // numpad 0
+    98: 'down',     // numpad 2
+    100: 'left',    // numpad 4
+    102: 'right',   // numpad 6
+    104: 'up',      // numpad 8
+    120: 'debug'    // F9
 };
 
 /**
