afb17f1328ad11369de421941e2782896a64d070
Anda <ax.fiolle@gmail.com>
2021-06-01 18:47:19 +0200

    SYNCRONIZED NPCs now have collisions ! :fire:
    + Mocked RPG Maker logic to resolve tiles collisions

--- server/core/gameworld.js
+++ server/core/gameworld.js
@@ -13,6 +13,7 @@ var exports = module.exports = {}
 world.gameMaps         = []; // Formated exploitable files from gamedata
 world.instanceableMaps = []; // Formated maps to track players and npcs
 world.instancedMaps    = []; // Maps that are currently up and synced
+world.tileSets         = []; // Needed to test collisions
 
 // Global function
 world.getMapById         = (mapId) => world.gameMaps.find(map => map.id === mapId);
@@ -29,9 +30,15 @@ world.getNpcByUniqueId  = (uniqueId) => world.getNpcInstance(uniqueId).npcsOnMap
 world.getAllNpcsByInstance = (id) => world.findInstanceById(id) && world.findInstanceById(id).npcsOnMap || [];
 
 world.initialize = () => {
+  world.fetchTilesets();
   world.fetchMaps(); // Load gamedata maps
   world.fetchInstances(); // Filter, format and save online maps
   console.log('[WORLD] # World is ready');
+}
+
+world.fetchTilesets = () => {
+  world.tileSets = MMO_Core["gamedata"].data['Tilesets'] || [];
+  console.log('[WORLD] # Tilesets Loaded')
 }
 
 world.fetchMaps = () => {
@@ -56,6 +63,7 @@ world.getDatasFromGameFile = (gameMap, fileName) => {
   return Object.assign(gameMap, {
     id: Number(fileName.slice(3)),
     fileName,
+    allNpcs: []
   });
 }
 
@@ -73,6 +81,7 @@ world.makeInstance = (map) => {
     npcsOnMap: [], // Array of Objects
     playersOnMap: [], // Array of String
     actionsOnMap: [], // Array of Objects -> Actions currently running in instance
+    allTiles: world.provideMapTiles(map), // Generate the map's tiles informations
   });
 }
 
@@ -121,6 +130,7 @@ world.fetchNpcsFromMap = (map) => {
   if (!map || !world.isMapInstanced(map.id)) return;
   for (let npc of world.findInstanceById(map.id).events.filter(event => JSON.stringify(event).includes('<Sync>'))) {
     const _generatedNpc = world.makeConnectedNpc(npc,map);
+    world.getMapById(map.id).allNpcs.push( world.makeConnectedNpc(npc,map,0,true) );
     if (_generatedNpc) {
       world.findInstanceById(map.id).npcsOnMap.push( _generatedNpc );
       console.log('[WORLD] Added synced NPC ' + _generatedNpc.uniqueId + ' on map ' + map.id);
@@ -128,13 +138,13 @@ world.fetchNpcsFromMap = (map) => {
   }
 }
 
-world.makeConnectedNpc = (npc,instance,pageIndex) => {
+world.makeConnectedNpc = (npc,instance,pageIndex,force) => {
   if (!npc || !instance) return;
   // Target selected or first page to assign helpers :
   const formatedPageIndex = (pageIndex && !isNaN(pageIndex)) ? parseInt(pageIndex) : 0;
   const _page = npc.pages && npc.pages[formatedPageIndex] || npc.pages[0];
   // If page contains a comment with "<Sync>" as parameter, return ConnectedNpc :
-  if (_page.list.find(l => l.code === 108 && l.parameters.find(p => p.toUpperCase().includes('<SYNC>')))) {
+  if (force || _page.list.find(l => l.code === 108 && l.parameters.find(p => p.toUpperCase().includes('<SYNC>')))) {
     return Object.assign(npc, {
       uniqueId: `@${instance.id}#${instance.npcsOnMap.length}?${npc.id}`, // Every NPC has to be clearly differentiable
       eventId: npc.id, // Event "ID" client-side
@@ -189,10 +199,11 @@ world.handleNpcTurn = (npc,currentTime,cooldown) => {
 
   if (npc._moveType === 1 && canMoveThisTurn) {
     const didMove = world.npcMoveRandom(world.getNpcByUniqueId(npc.uniqueId));
-    if (didMove) world.getNpcByUniqueId(npc.uniqueId).lastMoveTime = new Date();
-    // console.log('[WORLD] handleNpcTurn', npc.uniqueId, world.getNpcByUniqueId(npc.uniqueId).lastMoveTime);
+    if (didMove) {
+      world.getNpcByUniqueId(npc.uniqueId).lastMoveTime = new Date();
+      // console.log('[WORLD] handleNpcTurn', npc.uniqueId, world.getNpcByUniqueId(npc.uniqueId).lastMoveTime);
+    }
   }
-  
 }
 
 world.startInstanceLifecycle = (instanceId) => {
@@ -208,7 +219,7 @@ world.startInstanceLifecycle = (instanceId) => {
 
     world.findInstanceById(instanceId).paused = false; // Flag as running
     // _instance.actionsOnMap.map(action => world.handleInstanceAction(action, _instance, currentTime)); // Play Actions
-    world.findInstanceById(instanceId).npcsOnMap.map(npc => world.handleNpcTurn(npc, currentTime, 1000 * (npc._moveFrequency + 1))); // Animate NPCS
+    world.findInstanceById(instanceId).npcsOnMap.map(npc => world.handleNpcTurn(npc, currentTime, 6000 - (1000 * (npc._moveFrequency + 1)))); // Animate NPCS
 
     if (!world.findInstanceById(instanceId).playersOnMap.length) { // If no players on map at tick :
       setTimeout(() => {
@@ -232,30 +243,29 @@ world.npcCanPass = (npc, direction) => {
     y: npc.y
   };
   const _mapId = world.getNpcMapId(npc.uniqueId)
-  const x2 = world.getCoordsAfterMove(_coords, direction).x;
-  const y2 = world.getCoordsAfterMove(_coords, direction).y;
+  const x2 = world._roundXWithDirection(_mapId,_coords.x, direction);
+  const y2 = world._roundYWithDirection(_mapId,_coords.y, direction);
   if (!world._isValid(_mapId, x2, y2)) {
-      return false;
+    // console.log(npc.uniqueId, '!world._isValid(_mapId, x2, y2)', _mapId, x2, y2)
+    return false;
   }
-  if (!world._isMapPassable(_mapId, npc.x, npc.y, direction)) {
-      return false;
+  if (!world._isMapPassable(_mapId, _coords.x, _coords.y, direction)) {
+    // console.log(npc.uniqueId, '!world._isMapPassable(_mapId, _coords.x, _coords.y, direction)', _mapId, _coords.x, _coords.y, direction)
+    return false;
   }
   if (world._isCollidedWithCharacters(_mapId, x2, y2)) {
-      return false;
+    // console.log(npc.uniqueId, 'world._isCollidedWithCharacters(_mapId, x2, y2)', _mapId, x2, y2)
+    return false;
   }
   return true;
 };
 
 world.npcMoveStraight = (npc,direction,animSkip) => {
-  // console.log('[WORLD] npcMoveStraight (1/2)', npc.uniqueId, { x: npc.x,y: npc.y }, direction);
+  // console.log('[WORLD] npcMoveStraight (1/2)', npc.uniqueId, { x: npc.x,y: npc.y }, {direction});
   if (world.npcCanPass(npc,direction)) {
-    const _coords = {
-      x: npc.x,
-      y: npc.y
-    };
-    world.getNpcByUniqueId(npc.uniqueId).x = world.getCoordsAfterMove(_coords, direction).x;
-    world.getNpcByUniqueId(npc.uniqueId).y = world.getCoordsAfterMove(_coords, direction).y;
     const _map = world.getNpcInstance(npc.uniqueId);
+    world.getNpcByUniqueId(npc.uniqueId).x = world._roundXWithDirection(_map.id, npc.x, direction);
+    world.getNpcByUniqueId(npc.uniqueId).y = world._roundYWithDirection(_map.id, npc.y, direction);
     MMO_Core["socket"].emitToAll("npc_moving", {
       uniqueId: npc.uniqueId,
       mapId: _map.id,
@@ -265,7 +275,7 @@ world.npcMoveStraight = (npc,direction,animSkip) => {
       direction: direction,
       x: world.getNpcByUniqueId(npc.uniqueId).x,
       y: world.getNpcByUniqueId(npc.uniqueId).y,
-      skip: animSkip || false
+      skip: animSkip || false,
     });
     // console.log('[WORLD] npcMoveStraight (2/2)', npc.uniqueId, { x: world.getNpcByUniqueId(npc.uniqueId).x,y: world.getNpcByUniqueId(npc.uniqueId).y });
     return true;
@@ -277,23 +287,48 @@ world.npcMoveRandom = (npc) => {
   return world.npcMoveStraight(npc, direction);
 };
 
-world.getCoordsAfterMove = (coords,direction) => {
-  // coords: { x: number, y: number }
-  // direction : 2 = down, 4 = left, 6 = right, 8 = up
-  if (!coords || !direction) return;
-  const _XY = coords;
-  if (direction === 2) _XY.y += .5;
-  if (direction === 4) _XY.x -= 1;
-  if (direction === 6) _XY.x += 1;
-  if (direction === 8) _XY.y -= .5;
-  return _XY;
-  // TODO : 8 directions (1,3,7,9)
-}
 world.getReverseDir = (direction) => {
+  if (direction === 1) return 9;
   if (direction === 2) return 8;
+  if (direction === 3) return 7;
   if (direction === 4) return 6;
   if (direction === 6) return 4;
+  if (direction === 7) return 3;
   if (direction === 8) return 2;
+  if (direction === 9) return 1;
+  return false;
+}
+
+world.provideMapTiles = (map) => {
+  const grid = [
+    [ // x: [ y, ]
+      [], // y: [A,B,C,R]
+    ],
+  ];
+  const _data = map.data || [];
+  const _width = map.width; // Limit the iteration in horizontal
+  const _height = map.height; // Paginate the iteration in vertical (handle layers)
+  let heightIndex = 0, widthIndex = 0;
+
+  for (let dataIndex = 0; dataIndex < _data.length; dataIndex++) { // i = cell xy informations by layer
+    if (!grid[widthIndex]) grid[widthIndex] = [[]]; // if no X yet
+    if (!grid[widthIndex][heightIndex]) grid[widthIndex][heightIndex] = []; // if no Y yet
+    grid[widthIndex][heightIndex] = [_data[dataIndex]].concat(grid[widthIndex][heightIndex]); // Add to tile layers
+
+    if (widthIndex + 1 < _width) { // if still on current line
+      widthIndex++; // next cell
+    } else { 
+      heightIndex++; // next line
+      widthIndex = 0; // first cell
+      // (if next): layer first row, (else): next row
+      if (heightIndex >= _height) heightIndex = 0;
+    }
+  }
+  return grid;
+}
+world.mapTileFinder = (mapId,x,y) => {
+  // console.log('mapTileFinder', mapId, x, y);
+  return world.findInstanceById(mapId).allTiles[x][y];
 }
 
 world._isValid = (mapId,targetX,targetY) => {
@@ -303,16 +338,84 @@ world._isValid = (mapId,targetX,targetY) => {
   return true;
 }
 world._isMapPassable = (mapId,x,y,d) => {
-  const x2 = world.getCoordsAfterMove({x,y}, d);
-  const y2 = world.getCoordsAfterMove({x,y}, d);
+  const x2 = world._roundXWithDirection(mapId,x, d);
+  const y2 = world._roundYWithDirection(mapId,y, d);
   const d2 = world.getReverseDir(d);
-  return true; // world._isPassable(mapId,x, y, d) && world._isPassable(mapId,x2, y2, d2);
+  return world._isPassable(mapId,x, y, d) && world._isPassable(mapId,x2, y2, d2);
+}
+world._isPassable = (mapId,x,y,d) => {
+  return world._checkPassage(mapId, x, y, (1 << (d / 2 - 1)) & 0x0f);
+};
+
+world._roundX = function(mapId,x) {
+  const _map = world.findInstanceById(mapId);
+  return (_map.scrollType === 2 || _map.scrollType === 3) ? x % _map.width : x;
+};
+world._roundY = function(mapId,y) {
+  const _map = world.findInstanceById(mapId);
+  return (_map.scrollType === 2 || _map.scrollType === 3) ? y % _map.height : y;
+};
+world._xWithDirection = function(x, d) {
+  return x + (d === 6 ? 1 : d === 4 ? -1 : 0);
+};
+world._yWithDirection = function(y, d) {
+  return y + (d === 2 ? 1 : d === 8 ? -1 : 0);
+};
+world._roundXWithDirection = function(mapId, x, d) {
+  return world._roundX(mapId, x + (d === 6 ? 1 : d === 4 ? -1 : 0));
+};
+world._roundYWithDirection = function(mapId, y, d) {
+  return world._roundY(mapId, y + (d === 2 ? 1 : d === 8 ? -1 : 0));
+};
+
+world.__tilesetId = (mapId,x,y) => {
+  return world._layeredTiles(mapId, x, y);
+}
+world._tilesetFlags = (mapId) => {
+  const tileset = world.tileSets[world.getMapById(mapId).tilesetId];
+  if (tileset) {
+    return tileset.flags;
+  } else {
+    return [];
+  }
+}
+world._tileId = (mapId,x,y,z) => {
+  const _map = world.findInstanceById(mapId);
+  const width = _map.width;
+  const height = _map.height;
+  return _map.data[(z * height + y) * width + x] || 0;
+}
+world._layeredTiles = (mapId,x,y) => {
+  const tiles = [];
+  for (let i = 0; i < 4; i++) {
+      tiles.push(world._tileId(mapId, x, y, 3 - i));
+  }
+  return tiles;
+}
+world._checkPassage = (mapId,x,y,bit) => {
+  const flags = world._tilesetFlags(mapId);
+  const tiles = world.mapTileFinder(mapId,x,y);
+  for (const tile of tiles) {
+    const flag = flags[tile];
+    if ((flag & 0x10) !== 0) {
+      // [*] No effect on passage
+      continue;
+    }
+    if ((flag & bit) === 0) {
+      // [o] Passable
+      return true;
+    }
+    if ((flag & bit) === bit) {
+      // [x] Impassable
+      return false;
+    }
+  }
+  return false;
 }
 world._isCollidedWithCharacters = (mapId,x,y) => {
-  if (!world.isMapInstanced(mapId)) return true; // return collide to prevent move
-  // for (let userId of world.findInstanceById(mapId).playersOnMap) {
-  //   const _player = MMO_Core["database"].findUserById(userId);
-  //   if (_player.x === x && _player.y === y) return true;
-  // }
+  if (!world.getMapById(mapId)) return true; // return collide to prevent move
+  for (let _npc of world.getMapById(mapId).allNpcs) {
+    if (_npc.x === x && _npc.y === y) return true;
+  }
   return false;
 }
