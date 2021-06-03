98bf8a9b2d5ba78ada361a64cb13d6d9459b64f6
Anda <ax.fiolle@gmail.com>
2021-06-03 19:59:34 +0200

    Admin can /listNpc to get all spawned IDs
    i. useful for remove

---
+++ server/modules/messages/listNpc.js
@@ -0,0 +1,25 @@
+/* global MMO_Core */
+exports.initialize = function() {
+    exports.use = async function(args, initiator) {
+        if (initiator.playerData.permission < 100) {
+            return MMO_Core.socket.modules.messages.sendToPlayer(initiator, "System", "You don't have the permission to use this command.", "error");
+        }
+        if (!MMO_Core["gameworld"].getSummonMap()) {
+            return MMO_Core.socket.modules.messages.sendToPlayer(player, "System", "The server has no spawnMap.", "error");
+        }
+
+        const _print = (string) => MMO_Core.socket.modules.messages.sendToPlayer(initiator, "System", string, "action")
+
+        const idList = MMO_Core["gameworld"].spawnedUniqueIds;
+        _print("Spawned NPC List :");
+
+        for (let index in idList) {
+            const uniqueId = idList[index]
+            const _npc = MMO_Core["gameworld"].getNpcByUniqueId(uniqueId);
+            const mapId = _npc.mapId;
+            const x = _npc.x;
+            const y = _npc.y;
+            _print(`[${index}] on Map ${mapId} at (X ${x};Y ${y})`);
+        }
+    };
+};
