ed14e7a7c8d2bbfe1fabcb5098b5c0d3de33a9d9
Mourad Kejji <mkejji@gmail.com>
2022-01-23 04:42:43 +0100

    Moved server host and port to .env + some cleanup
    - Removed deprecated scripts (.sh files)
    - Switched to yarn lockfile
    - Added some yarn scripts
    - applied eslint rules on modified files
    
    Closes #3

--- unix-start.sh
+++
@@ -1,4 +0,0 @@
-rm package.json &&
-cp package.json.example package.json &&
-npm install &&
-npm start
--- windows-start.bat
+++
@@ -1,4 +0,0 @@
-del "./package.json"
-xcopy "./package.json.example" "./package.json"
-call npm install
-call npm start 
